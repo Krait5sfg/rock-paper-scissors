c9a0fd2b1448b8dbfbbcfa2b8080007f993a1d3f
Yura <yurii_kulachenko@mail.ru>
2019-12-28 22:44:11 +0300

    change game

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 'use strict';
+//game setings
+let scoreCom = 0;
+let scorePl = 0;
+let rounds = null;
+
+//container DOM for game
+const gameContainer = document.querySelector('.container');
+
+//begin game - message for player and input for player choice number of rounds
+//create new Elements
+const messageForPlayer = document.createElement('p');
+messageForPlayer.textContent = 'Start game. Choose your number of rounds: ';
+const numberOfRounds = document.createElement('input');
+numberOfRounds.setAttribute('type', 'text');
+//add new Element in DOM
+gameContainer.appendChild(messageForPlayer);
+gameContainer.appendChild(numberOfRounds);
+numberOfRounds.focus();
+
+//get player choice number of rounds and start game
+numberOfRounds.addEventListener('change', () => {
+    messageForPlayer.textContent = 'Game begin';
+    gameContainer.removeChild(numberOfRounds);
+    rounds = +numberOfRounds.value;
+    //start game
+    playRound(gameContainer, rounds);
+
+});
 
 function computerPlay() {
     let numRandom = Math.ceil(Math.random() * 3);
@@ -16,65 +44,108 @@ function computerPlay() {
 }
 
 
-function playRound(playerSelection, computerSelection) {
-    let plSel = playerSelection.toLowerCase().slice(0, 1);
-    plSel = plSel.toUpperCase();
-    plSel = plSel.concat('', playerSelection.toLowerCase().slice(1, playerSelection.length));
+function playRound(container, gameLength) {
+    let result = '';
+    let count = 1;
 
-    let comSel = computerSelection();
-    console.log(`player selection - ${plSel} and computer selection - ${comSel}`);
-    switch (true) {
-        case (plSel == 'Rock' && comSel == 'Paper'):
-            return "You Lose! Paper beats Rock";
-        case (plSel == 'Rock' && comSel == 'Scissors'):
-            return "You Win! Rock beats Scissors";
-        case (plSel == 'Rock' && comSel == 'Rock'):
-            return "Drawn game! Rock equal Rock";
+    //new buttons elements and message elements
+    let choiceArray = ['Rock', 'Paper', 'Scissors'];
+    const btnArray = [];
+    let messageInGameProcess = document.createElement('p');
+    let messageForRounds = document.createElement('p');
 
-        case (plSel == 'Paper' && comSel == 'Rock'):
-            return "You Win! Paper beats Rock";
-        case (plSel == 'Paper' && comSel == 'Scissors'):
-            return "You Lose! Scissors beats Paper";
-        case (plSel == 'Paper' && comSel == 'Paper'):
-            return "Drawn game! Paper equal Paper";
+    //count 
+    messageForRounds.textContent = `Round: ${count}`;
+    container.appendChild(messageForRounds);
 
-        case (plSel == 'Scissors' && comSel == 'Rock'):
-            return "You Lose! Rock beats Scissors";
-        case (plSel == 'Scissors' && comSel == 'Paper'):
-            return "You Win! Scissors beats Paper";
-        case (plSel == 'Scissors' && comSel == 'Scissors'):
-            return "Drawn game! Scissors equal Scissors";
+    for (let i = 0; i < choiceArray.length; i++) {
+
+        //create new elements
+        btnArray[i] = document.createElement('button');
+        btnArray[i].setAttribute('data-info', choiceArray[i]);
+        btnArray[i].textContent = `${choiceArray[i]}`;
+        container.appendChild(btnArray[i]);
+
+        //game process
+        btnArray[i].addEventListener('click', function () {
+            let plSel = this.getAttribute('data-info');
+            let comSel = computerPlay();
+
+
+
+            //define winer
+            switch (true) {
+                case (plSel == 'Rock' && comSel == 'Paper'):
+                    result = "You Lose! Paper beats Rock";
+                    break;
+                case (plSel == 'Rock' && comSel == 'Scissors'):
+                    result = "You Win! Rock beats Scissors";
+                    break;
+                case (plSel == 'Rock' && comSel == 'Rock'):
+                    result = "Drawn game! Rock equal Rock";
+                    break;
+
+                case (plSel == 'Paper' && comSel == 'Rock'):
+                    result = "You Win! Paper beats Rock";
+                    break;
+                case (plSel == 'Paper' && comSel == 'Scissors'):
+                    result = "You Lose! Scissors beats Paper";
+                    break;
+                case (plSel == 'Paper' && comSel == 'Paper'):
+                    result = "Drawn game! Paper equal Paper";
+                    break;
+
+                case (plSel == 'Scissors' && comSel == 'Rock'):
+                    result = "You Lose! Rock beats Scissors";
+                    break;
+                case (plSel == 'Scissors' && comSel == 'Paper'):
+                    result = "You Win! Scissors beats Paper";
+                    break;
+                case (plSel == 'Scissors' && comSel == 'Scissors'):
+                    result = "Drawn game! Scissors equal Scissors";
+                    break;
+            }
+
+            if (result.search('You Win') === 0) {
+                scorePl++;
+            } else if (result.search('Drawn game') === 0) {
+                scoreCom++;
+                scorePl++;
+            } else if (result.search('You Lose') === 0) {
+                scoreCom++;
+            }
+
+            //message who winner and about player and computer selection
+            messageInGameProcess.textContent = `Player selection - ${plSel} and computer selection - ${comSel}.`;
+            messageInGameProcess.textContent += ` ${result}. Player count: ${scorePl}; Computer count: ${scoreCom}`;
+            container.appendChild(messageInGameProcess);
+
+            //count
+            count++;
+
+            if (count > gameLength) {
+                //end game
+                container.removeChild(messageForRounds);
+
+                if (scorePl > scoreCom) {
+                    messageInGameProcess.textContent = `Player selection - ${plSel} and computer selection - ${comSel}. You Win! Player count: ${scorePl}; Computer count: ${scoreCom}`;
+                } else if (scorePl < scoreCom) {
+                    messageInGameProcess.textContent = `Player selection - ${plSel} and computer selection - ${comSel}. You Lose! Player count: ${scorePl}; Computer count: ${scoreCom}`;
+                } else if (scorePl == scoreCom) {
+                    messageInGameProcess.textContent = `Player selection - ${plSel} and computer selection - ${comSel}. Drawn game! Player count: ${scorePl}; Computer count: ${scoreCom}`;
+                }
+
+                messageForPlayer.textContent = 'End game!';
+
+            } else {
+                messageForRounds.textContent = `Round: ${count}`;
+            }
+
+        });
 
     }
 }
 
-function game(gameLength) {
-    let scoreCom = 0;
-    let scorePl = 0;
 
-    for (let x = 0; x < gameLength; x++) {
-        let playerSelection = window.prompt('Input Your choice: Rock, Paper or Scissors');
-        let result = playRound(playerSelection, computerPlay);
-        if (result.search('You Win') === 0) {
-            scorePl++;
-        } else if (result.search('Drawn game') === 0) {
-            scoreCom++;
-            scorePl++;
-        } else if (result.search('You Lose') === 0) {
-            scoreCom++;
-        }
-        console.log(`player score - ${scorePl}, computer score - ${scoreCom}`);
-    }
 
-    if (scorePl > scoreCom) {
-        console.log('You Win!');
-    } else if (scorePl < scoreCom) {
-        console.log('You Lose!');
-    } else if (scorePl == scoreCom) {
-        console.log('Drawn game!');
-    }
 
-}
-
-//start game 5 times
-game(5);
